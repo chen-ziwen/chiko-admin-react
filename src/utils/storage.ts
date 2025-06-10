4ffc9952bf50b496c8cce2d4c1ead40934a6c3d8
chenziwen <chenziwen@qesong.com>
2025-06-10 17:35:10 +0800

    feat: 新增 themeContext

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,9 +1,9 @@
-import { createLocalforage,createStorage, StorageType } from '@ca/utils';
+import { createLocalforage, createStorage, StorageType } from '@chiko-admin-react/utils';
 
 const storagePrxfix = import.meta.env.VITE_STORAGE_PREFIX || '';
 
-export const localStg = createStorage<StorageType.Local>('local',storagePrxfix);
+export const localStg = createStorage<StorageType.Local>('local', storagePrxfix);
 
-export const sessionStg = createStorage<StorageType.Session>('session',storagePrxfix);
+export const sessionStg = createStorage<StorageType.Session>('session', storagePrxfix);
 
 export const localforage = createLocalforage<StorageType.Local>("local");
