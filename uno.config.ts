941b411121917a72874ddf0d2c86ad7cb61c6cdb
chenziwen <chenziwen@qesong.com>
2025-06-17 14:36:30 +0800

    docs: 初始化基于 Docusaurus 的文档教程

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,9 @@
-import { presetChikoAdmin } from '@chiko-admin/uno-preset';
 import { type Theme, presetWind3 } from '@unocss/preset-wind3';
 import transformerDirectives from '@unocss/transformer-directives';
 import transformerVariantGroup from '@unocss/transformer-variant-group';
 import { defineConfig } from '@unocss/vite';
+
+import { presetChikoAdmin } from './build';
 
 export default defineConfig<Theme>({
   content: {
